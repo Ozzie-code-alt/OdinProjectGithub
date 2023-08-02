0d894b6985c3323bde4871cb580d74393f8c83ca
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-08-02 15:52:19 +0800

    Object COnstructors

--- jsExcercise.js
+++ jsExcercise.js
@@ -1,1 +1,17 @@
-console.log("Hehe")
+function Book (title, author, pages, isRead){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+
+    this.information = function(){
+        console.log(title, author , pages, isRead)
+    }
+
+}
+
+const Book1 = new Book("The Hobbit", "Justin Santos", 17, "Yep")
+
+console.log(Book1.information())
+
+
